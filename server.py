81dcd5ba6f0dcc869bf9b65bc8e0bf4c7e3a21ec
Marvin <marvin.voss@t-online.de>
2023-04-25 08:05:02 +0200

    changed server folder name

--- server.py
+++ server.py
@@ -1,10 +1,4 @@
-#
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
-#
-
-import time 
+import time
 import threading
 import os 
 import re
@@ -46,7 +40,6 @@ cwd = os.getcwd()
 print(">>>>> Init NN") 
  
 outpath = "/output_MATestImages"
-#p = os.system("bin/predict.py model.path=$(pwd)/lama-places/lama-fourier indir=$(pwd)/fromdevice outdir=$(pwd)/output_MATestImages")
 @hydra.main(config_path='configs/prediction', config_name='default.yaml')
 def main(predict_config: OmegaConf):
     print(">>>>> Starting Server") 
@@ -64,9 +57,6 @@ message = ""
 data = ""
 prefix = ""
 conn, addr = _socket.accept()
-
-
-
 
 
 def recv_timeout(the_socket,buffsize,timeout=0.1):
@@ -128,17 +118,6 @@ def ReadoutData():
         #print("...read data message content is"+content[0:30]+" ... " + content[-100:] + "with prefix " + prefix)
         return prefix, content, True	
 
-	# try:
-	# 	prefix = data.decode("utf-8").split(':')[0]
-	# 	message = data.decode("utf-8").split(':')[1]
-	# 	print("...read data rawmessage is  - message content is with prefix {2}.".format(data.decode("utf-8"),message,prefix))
-	# 	return prefix, message, True
-	# except IndexError:
-	# 	print("indexerror " + data.decode("utf-8"))
-	# 	return prefix, message, False
-	# 	#break;
-
-
 
 while True:
     #  Wait for next request from client
@@ -153,16 +132,12 @@ while True:
             conn.send("handshake:Connection successfull_".encode("ascii"))
             print(">>>>> Handshake Done")
         elif prefix == 'background':
-            #print('CONTENT##'+str(content)+'##ENDCONTENT')
             background = Image.open(BytesIO(base64.b64decode(content)))
-            #background = Image.open(BytesIO(base64.b64decode(str(content))))
-            #background.show()
             background.save("fromdevice/background.jpg")
             conn.send("background:Background received successfull_".encode("ascii"))
             print(">>>>> Background Received") 
         elif prefix == 'mask':
             mask = Image.open(BytesIO(base64.b64decode(content)))
-            #mask.show()
             mask.save("fromdevice/background_mask.png")
             conn.send("mask:Mask received successfull_".encode("ascii"))
             print(">>>>> Mask Received")
@@ -170,32 +145,6 @@ while True:
             print(">>>>> Unity Closed Restarting Server...") 
             os.execv(sys.executable, ['python'] + sys.argv)
     readoutdone = False;
-    #case = message[:10]
-    # if not handshake:
-    # 	handshake = True;
-    # 	print("Client Connected with message {0}".format(message))
-    # 	conn.send(b"SERVER: connection established")
-    # 	message = "";
-    # print("Waiting for background...")
-    # if background is None and message is not None:
-    # 	#print("Received Backgroundrequest %s" % message)
-    # 	# image converted as string
-    # 	background = Image.open(BytesIO(base64.b64decode(message)))
-    # 	print("Received request: %s" % background)
-    # 	conn.send(b"SERVER Received request: background")
-    # 	message = "";
-    # 	#background.show()
-    #  	#background.save("fromdevice/background.png") 
-    # print("Waiting for Mask...")
-    # #elif mask is None and message is not None:
-     #   image_data = message # byte values of the image
-      #  mask = Image.open(io.BytesIO(image_data))
-       # mask.show()
-        #mask.save("fromdevice/background_mask.png")
-        #print("Received request: %s" % mask)
-        #_socket.send(b"Received request: mask")
-    #elif callforImage is None and message is not None:
-    #	callforImage = message;
 
     if(background is not None and mask is not None):
         background = None;
@@ -204,8 +153,7 @@ while True:
         predict.doinpaint()
         endtime = time.time();
         print(endtime-starttime)
-        for filename in glob.iglob('output_MATestImages/*.png', recursive=True):
-        #for filename in glob.iglob('fromdevice/background.png', recursive=True):
+        for filename in glob.iglob('output/*.png', recursive=True):
             im = Image.open(filename, "r")
             #im.show()
             buffered = BytesIO()
@@ -217,14 +165,3 @@ while True:
 
             tosend = prefix+image+subfix
             conn.send(tosend)
-            #print("Send Result - start waiting again..." +tosend.decode("ascii")[-50:])
- 
-    #  Do some 'work'.
-    #  Try reducing sleep time to 0.01 to see how blazingly fast it communicates
-    #  In the real world usage, you just need to replace time.sleep() with
-    #  whatever work you want python to do, maybe a machine learning task?
-    # time.sleep(1)
-    #  Send reply back to client
-    #  In the real world usage, after you finish your work, send your output here
- 
-    #socket.send(b"Received")
